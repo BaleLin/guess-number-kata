f9ddd1ea1360a672a0724da0e35f2ce05dbf901a
BaleLin <bale.lin@oocl.com>
2018-07-12 19:35:10 +0800

    测试数据验证方法

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -2,6 +2,12 @@ package tw.core;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tw.core.exception.AnswerFormatIncorrectException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -13,5 +19,35 @@ public class AnswerTest {
     public void setUp() {
         actualAnswer = Answer.createAnswer("1 2 3 4");
     }
+    @Test
+    public void should_call_a_successful_message_given_a_validate_date(){
+        //given
+        try{
+            actualAnswer.validate();
+        }catch (AnswerFormatIncorrectException exception){
+            exception.printStackTrace();
+        }
+    }
+    public void should_call_a_fail_message_given_a_unvalidate_date(){
+        //given
+        actualAnswer = Answer.createAnswer("3 2 3 4");
+        //when
+        try{
+            actualAnswer.validate();
+            fail("should fial,but not");
+        }catch (AnswerFormatIncorrectException exception){
 
+        }
+    }
+    public void should_call_success_message_given_a_sameIndex_date(){
+        //given
+        actualAnswer = Answer.createAnswer("3 2 3 4");
+        //when
+        try{
+            actualAnswer.validate();
+            fail("should fial,but not");
+        }catch (AnswerFormatIncorrectException exception){
+
+        }
+    }
 }
